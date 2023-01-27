120452b5c4bd4c9066653b8da6b34fe7d06daaf0
User <askarmustafaev015@gmail.ru>
2023-01-27 18:54:09 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hm_2_3/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.hm_2_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fr_1, new Fragment());
+        }
+    }
+}
